cca6253a126e0ce759d4825415115d28a8413363
Filip Tsolov <filip.tsolov@code.berlin>
2021-05-29 14:26:56 +0200

    added generating on space press

--- src/App.tsx
+++ src/App.tsx
@@ -15,29 +15,37 @@ const App = () => {
 
   function onKeyDown(e: any) {
     console.log(e.code);
+    if (e.code === "Space") {
+      handleButtonClick();
+    }
   }
-  console.log(currentQuoteObject);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    document.body.addEventListener("keydown", onKeyDown);
+  }, []);
+
   const twitterLink = `https://twitter.com/intent/tweet?text="${currentQuoteObject.quote}" - ${currentQuoteObject.author}`;
 
   return (
-    <div className="card" onKeyDown={onKeyDown}>
-      <h2>{currentQuoteObject.quote}</h2>
-      <p>- {currentQuoteObject.author}</p>
-      <div className={"buttons"}>
-        <a
-          className="twitter bouncy"
-          href={twitterLink}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i className="fab fa-twitter" />
-        </a>
-        <button onClick={handleButtonClick}>
-          <i className="fas fa-redo-alt" /> New quote!
-        </button>
+    <div>
+      <div className="card">
+        <h2>{currentQuoteObject.quote}</h2>
+        <p className="author">- {currentQuoteObject.author}</p>
+        <div className={"buttons"}>
+          <a
+            className="twitter bouncy"
+            href={twitterLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-twitter" />
+          </a>
+          <button onClick={handleButtonClick}>
+            <i className="fas fa-redo-alt" /> New quote!
+          </button>
+        </div>
       </div>
+      <p className="hint">Hint: Press spacebar to get a new quote!</p>
     </div>
   );
 };
